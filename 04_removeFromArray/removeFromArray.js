774fd42bc5f5fef8ff7b52d106226d85315f7aa7
Cdunbar40 <cdunbar1240@gmail.com>
2024-03-09 13:52:13 -0500

    Completed JS Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    
+    var output = array.filter((arg) => args.findIndex((element) => arg === element) == -1);
+    return output;
 };
 
 // Do not edit below this line
