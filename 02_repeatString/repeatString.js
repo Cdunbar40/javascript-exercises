e4241c007cebd5c7c5bbacef33d580be231a0d53
Cdunbar40 <cdunbar1240@gmail.com>
2024-03-09 13:12:20 -0500

    Completed JS Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,reps) {
+  if (reps < 0){
+    return 'ERROR'
+  }
+  var output = '';
+  
+  for (var i=0;i < reps;i++){
+    output += string;
+  }
 
+  return output;
 };
 
 // Do not edit below this line
