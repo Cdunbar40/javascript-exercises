c65bb3c7ee520ea7be30d347659c77eb5ee77606
Cdunbar40 <cdunbar1240@gmail.com>
2024-03-11 19:14:37 -0400

    Completed JS exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((typeof num1 != 'number')||
+        (typeof num2 != 'number')||
+        (num1 < 0) || (num2 < 0)) {
+            return 'ERROR';
+        }
 
+    
+    var sum = 0;
+    for (var i = (num1 < num2 ? num1:num2); i < (num1 < num2 ? num2+1:num1+1); i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
