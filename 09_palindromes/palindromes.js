540582319701f81d5d402528c7a65c3e04cef2ab
Cdunbar40 <cdunbar1240@gmail.com>
2024-03-20 18:56:14 -0400

    Completed JS Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = Array.from(string).filter((char) =>  (char.toLowerCase() != char.toUpperCase()) || Number.isInteger(Number.parseInt(char)));
+    let forward = arr.join('').toLowerCase();
+    let rev = arr.reverse().join('').toLowerCase();
+    return forward == rev;
 };
 
 // Do not edit below this line
